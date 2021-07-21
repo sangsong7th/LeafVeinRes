ba9a2aa278759ae3e69dcc1d39bbd6644bad6869
sangsong7th <13833654292@163.com>
2021-07-21 22:10:55 +0800

    提交目录实体

---
+++ src/main/java/enity/CatalogueConfigEnity.java
@@ -0,0 +1,26 @@
+package enity;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.List;
+
+/**
+ * <P>目录文件实体</P>
+ * @author sangsong6th
+ * @since  2021/7/19
+ */
+public class CatalogueConfigEnity {
+    /**
+     * 目录名称
+     */
+    private String catalogueName;
+    /**
+     * 文件目录分支
+     */
+    private JSONObject jsonObject;
+    /**
+    是否需要更新
+     */
+    private boolean bolnUpdata;
+
+}
